9e2bcb97a91d4a3a53fbb6941d81ba4c1cf7b06a
Adam Gray <68389728+asg-images@users.noreply.github.com>
2022-04-18 11:56:31 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const inputs = document.querySelectorAll("input");
+const mphValue = document.querySelector(".inputMph");
+const kmhValue = document.querySelector(".inputKmh");
+const knotsValue = document.querySelector(".inputKnots");
+const machValue = document.querySelector(".inputMach");
+const error = document.querySelector(".error");
+
+function showError(){
+  error.classList.add("active");
+}
+
+function clearError(){
+  error.classList.remove("active")
+}
+
+inputs.forEach(input => {
+  input.addEventListener("input", function(e){
+    const inputValue = e.target;
+
+    if(isNaN(inputValue.value)){
+      showError();
+      setTimeout(clearError, 4000)
+      return;
+    };
+
+    switch(inputValue){
+      case mphValue:
+        kmhValue.value = Math.round(inputValue.value * 1.609344) + " Kmh";
+        knotsValue.value = Math.round(inputValue.value / 1.151) + " Knots";
+        machValue.value = Math.round(inputValue.value / 767) + " Mach";
+      break;
+      case kmhValue:
+        mphValue.value = Math.round(inputValue.value / 1.609344) + " Mph";
+        knotsValue.value = Math.round(inputValue.value / 1.852) + " Knots";
+        machValue.value = Math.round(inputValue.value / 1235) + " Mach";
+      break;
+      case knotsValue:
+        kmhValue.value = Math.round(inputValue.value * 1.852) + " Kmh";
+        mphValue.value = Math.round(inputValue.value * 1.151) + " Mph";
+        machValue.value = Math.round(inputValue.value / 667) + " Mach";
+      break;
+      case machValue:
+        kmhValue.value = Math.round(inputValue.value * 1235) + " Kmh";
+        knotsValue.value = Math.round(inputValue.value * 667) + " Knots";
+        mphValue.value = Math.round(inputValue.value * 767) + " Mph";
+      break;
+    }
+  })
+})
